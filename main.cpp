c43b83f145f93b2f51f991dd2f226a0230b4e52a
piotrsamczuk <samczukp@gmail.com>
2022-10-12 22:33:11 +0200

    main empty for testing

--- main.cpp
+++ main.cpp
@@ -3,18 +3,5 @@
 
 int main()
 {
-    DoublyLinkedList<std::string> list1;
-	list1.addBack("1");
-	list1.addBack("2");
-	list1.addBack("3");
-	list1.addBack("4");
-	list1.addBack("5");
-	list1.addBack("6");
-	list1.display();
-
-	//testing
-	list1.addFront("0");
-	list1.reverse();
-	list1.display();
 	return 0;
 }
